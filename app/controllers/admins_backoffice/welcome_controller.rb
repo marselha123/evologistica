bf9544ed18d4e09dd2e0f3d936d60f91e9128e45
Jackson Pires <jack@jack.com>
2024-10-08 00:17:39 +0000

    Configurando a task3

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -6,4 +6,37 @@ class AdminsBackoffice::WelcomeController < AdminsBackofficeController
   def show
     @producao = Producao.find(params[:id])
   end  
+
+  def new
+    @producao = Producao.new
+  end
+  
+  def create
+    @producao = Producao.new(params_producao)
+    if @producao.save
+      redirect_to admins_backoffice_producoes_path, notice: "Produção cadastrada com sucesso!"
+    else
+      render :new
+    end  
+  end  
+
+  def update
+    @producao = Producao.update(params_producao)
+    if @producao.save
+      redirect_to admins_backoffice_producoes_path, notice: "Produção atualizada com sucesso!"
+    else
+      render :edit
+    end  
+  end
+
+  def destroy
+    @producao = Producao.destroy
+    if @producao.save
+      redirect_to admins_backoffice_producoes_path, notice: "Produção excluída com sucesso!"
+    else
+      render :index
+    end  
+  end
+
+
 end
